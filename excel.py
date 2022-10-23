41f242cbee75c1086d8b751ffe20e4dbc19131bd
PlanetNamekTech <sr23513@gmail.com>
2022-10-22 19:45:29 -0700

    some properties to change sheet

--- excel.py
+++ excel.py
@@ -3,7 +3,15 @@ from openpyxl import Workbook, load_workbook
 
 wb = load_workbook('C2-W3-Practice-Challenge.xlsx')
 ws = wb.active
-print(ws)
+#Looks at the value of the cell
+print(ws['C15'].value)
+#If a change is made like below, the workbook needs to be saved
+ws['B2'].value = "Test" #can assign a value without using .value
+wb.save('C2-W3-Practice-Challenge.xlsx')
+#To create a new sheet
+wb.create_sheet('PythonSheet')
+wb.save('C2-W3-Practice-Challenge.xlsx')
+print(wb.sheetnames)
 
 
 
